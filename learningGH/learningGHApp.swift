f19c96ff6150e61fe0eb25a06805d7f689cc74d2
Benyamin <benykariminia@gmail.com>
2023-07-18 15:40:48 +0330

    Initial Commit

---
+++ learningGH/learningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  learningGHApp.swift
+//  learningGH
+//
+//  Created by Benyamin kariminia on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct learningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
